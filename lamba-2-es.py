9a7e3b92d62bff2f57881a568392706ef8bd6c6f
viral73 <viralpuja@gmail.com>
2021-03-08 08:44:49 -0500

    Add files via upload

--- lamba-2-es.py
+++ lamba-2-es.py
@@ -1,6 +1,7 @@
 import boto3
 import re
 import requests
+import json
 from requests_aws4auth import AWS4Auth
 
 region = 'us-east-1' # e.g. us-west-1
@@ -8,37 +9,46 @@ service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-host = 'https://vpc-vtex-4lw43cjbxjwej36vgggohk6ko4.us-east-1.es.amazonaws.com' # the Amazon ES domain, including https://
-index = 'lambda-s3-index'
-url = host + '/' + index
+host = 'https://<xyz>.us-east-1.es.amazonaws.com'
 
-headers = { "Content-Type": "application/json" }
+url = host + '/' + '_bulk'
+
+headers = { "Content-Type": "application/x-ndjson" }
 
 s3 = boto3.client('s3')
 
-# Regular expressions used to parse some simple log lines
-ip_pattern = re.compile('(\d+\.\d+\.\d+\.\d+)')
-time_pattern = re.compile('\[(\d+\/\w\w\w\/\d\d\d\d:\d\d:\d\d:\d\d\s-\d\d\d\d)\]')
-message_pattern = re.compile('\"(.+)\"')
 
 # Lambda execution starts here
 def handler(event, context):
-    for record in event['Records']:
-
+    es_bulk_payload = ""
+    for e in event['Records']:
+        records = json.loads(e["body"])
+        
+        for record in records["Records"]:
         # Get the bucket name and key for the new file
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
+            bucket = record['s3']['bucket']['name']
+            key = record['s3']['object']['key']
 
         # Get, read, and split the file into lines
-        obj = s3.get_object(Bucket=bucket, Key=key)
-        body = obj['Body'].read()
-        lines = body.splitlines()
-        
-        # Match the regular expressions to each line and index the JSON
-        for line in lines:
-            ip = ip_pattern.search(line).group(1)
-            timestamp = time_pattern.search(line).group(1)
-            message = message_pattern.search(line).group(1)
+            obj = s3.get_object(Bucket=bucket,Key=key)
+            body = obj['Body'].read().decode("utf-8")
+            #print(obj)
+            #print(body)
             
-            document = { "ip": ip, "timestamp": timestamp, "message": message }
-            r = requests.post(url, auth=awsauth, json=document, headers=headers)
+        # Extracting S3 object header data to ingest directly into ES
+            #metadata = s3.head_object(Bucket=bucket, Key=key)
+         
+            payload = json.loads(body)
+            
+            es_payload = list()
+            es_payload.append('{"index" : { "_index" : "<index-name>"} }')
+            
+            es_payload.append(json.JSONEncoder().encode(payload["summary"]))
+            #es_payload.append(metadata["ResponseMetadata"]["HTTPHeaders"]["x-amz-meta-summary"])
+            
+            es_bulk_payload += "\n".join(es_payload) + "\n"
+    #print(es_bulk_payload)
+            
+    r = requests.post(url, auth=awsauth, data=es_bulk_payload, headers=headers)
+    #payload = list()
+    print(r.text)
